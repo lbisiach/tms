4fa228661d8fda758bcddad88ed0f5a26f8f2bdb
Bisiach Lucio <127256579+lbisiach@users.noreply.github.com>
2025-01-26 13:14:51 -0300

    [MOD] Reset to draft supplier invoice and credit note

--- models/account_move.py
+++ models/account_move.py
@@ -10,3 +10,11 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     service_id = fields.Many2one('service.tms', string=_("Number of Service"), required=False)
+
+    @api.depends('restrict_mode_hash_table', 'state')
+    def _compute_show_reset_to_draft_button(self):
+        for move in self:
+            move.show_reset_to_draft_button = (
+                not move.restrict_mode_hash_table \
+                and (move.state == 'cancel' or (move.state == 'posted' and not move.need_cancel_request))
+            )
